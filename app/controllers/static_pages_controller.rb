19d9bb6c6835d07d414f1cc6f35b9872c7c53a0f
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-02-13 18:40:02 +0530

    added search functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,13 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def search
+    flickr = Flickr.new
+    unless params[:user_id].blank?
+      @photos = flickr.photos.search(user_id: params[:user_id])
+    else
+      @photos = flickr.photos.getRecent
+    end
+  end
+
 end
